7df07ded89ec0fe1fd4ee58a5b121136b193ac68
Jascha030 <contact@jaschavanaalst.nl>
2021-10-16 22:28:57 +0200

    Created ResourceService with UnitTests

--- src/Service/ResourceService.php
+++ src/Service/ResourceService.php
@@ -2,7 +2,46 @@
 
 namespace Jascha030\PM\Service;
 
+use Jascha030\PM\Project\Category\ResourceCategory;
+
 class ResourceService
 {
+    private array $definitions;
 
+    public function __construct()
+    {
+        $this->definitions = [];
+
+        $this->fetchDefinitions();
+    }
+
+    public function getDefinitions(): array
+    {
+        return $this->definitions;
+    }
+
+    public function fetchDefinitions(): void
+    {
+        $definitionsPath = dirname(__FILE__, 3) . '/config/definitions.php';
+
+        if (! file_exists($definitionsPath)) {
+            throw new \InvalidArgumentException('Definitions config is corrupted.');
+        }
+
+        $definitions = include $definitionsPath;
+
+        foreach ($definitions as $key => $definition) {
+            if (!is_array($definition)) {
+                throw new \InvalidArgumentException('Definitions config is corrupted.');
+            }
+
+            $options = $definition['options'];
+
+            if (! in_array('Other', $options, true)) {
+                $options[] = 'Other';
+            }
+
+            $this->definitions[$key] = new ResourceCategory($definition['name'], $key, $options);
+        }
+    }
 }
--- tests/Service/ResourceServiceTest.php
+++ tests/Service/ResourceServiceTest.php
@@ -2,10 +2,44 @@
 
 namespace Jascha030\PM\Test\Service;
 
+use Jascha030\PM\Project\Category\ResourceCategoryInterface;
 use Jascha030\PM\Service\ResourceService;
 use PHPUnit\Framework\TestCase;
 
 class ResourceServiceTest extends TestCase
 {
+    public function test__construct(): ResourceService
+    {
+        $service = new ResourceService();
+        self::assertInstanceOf(ResourceService::class, $service);
 
+        return $service;
+    }
+
+    /**
+     * @depends test__construct
+     */
+    public function testFetchDefinitions(ResourceService $service): array
+    {
+        self::assertIsArray($service->getDefinitions());
+        self::assertEmpty($service->getDefinitions());
+
+        $service->fetchDefinitions();
+        $definitions = $service->getDefinitions();
+
+        self::assertIsArray($definitions);
+        self::assertNotEmpty($definitions);
+
+        return $definitions;
+    }
+
+    /**
+     * @depends testFetchDefinitions
+     */
+    public function testGetDefinitions(array $definitions): void
+    {
+        foreach ($definitions as $definition) {
+            self::assertInstanceOf(ResourceCategoryInterface::class, $definition);
+        }
+    }
 }
