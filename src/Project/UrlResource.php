9b6beb588d35e1f96447587944cffe157179336c
Jascha030 <contact@jaschavanaalst.nl>
2021-10-16 22:27:53 +0200

    Added Url Resource

--- src/Project/UrlResource.php
+++ src/Project/UrlResource.php
@@ -2,7 +2,52 @@
 
 namespace Jascha030\PM\Project;
 
-class UrlResource
-{
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 
+class UrlResource extends ProjectResourceAbstract
+{
+    public const MAC   = 0;
+    public const LINUX = 1;
+
+    public const SUPPORTED_OS_UNAMES = [
+        self::MAC   => 'Darwin',
+        self::LINUX => 'Linux',
+    ];
+
+    private const OS_OPEN_COMMANDS = [
+        self::MAC   => 'open',
+        self::LINUX => 'xdg-open',
+    ];
+
+    public function callResource(InputInterface $input, OutputInterface $output): int
+    {
+        if (empty($this->getValue())) {
+            $output->writeln("Invalid url supplied for: {$this->getName()}.");
+
+            return Command::FAILURE;
+        }
+
+        if (! $this->supportsOS()) {
+            $output->writeln('This command currently does not seem to support your system.');
+
+            return Command::FAILURE;
+        }
+
+        $openCommand = $this->getOpenCommand();
+
+        return Process::fromShellCommandline("{$openCommand} {$this->getValue()}")->run();
+    }
+
+    final public function getOpenCommand(): string
+    {
+        return self::OS_OPEN_COMMANDS[array_flip(self::SUPPORTED_OS_UNAMES)[PHP_OS]];
+    }
+
+    private function supportsOS(): bool
+    {
+        return isset(array_flip(self::SUPPORTED_OS_UNAMES)[PHP_OS]);
+    }
 }
