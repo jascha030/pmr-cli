73994b653e4a4f69dfceaa1a06c6d157d87fac9b
Jascha030 <contact@jaschavanaalst.nl>
2021-10-16 22:28:40 +0200

    Started initCommand

--- src/Console/Command/InitCommand.php
+++ src/Console/Command/InitCommand.php
@@ -2,7 +2,25 @@
 
 namespace Jascha030\PM\Console\Command;
 
-class InitCommand
-{
+use Symfony\Component\Console\Command\Command;
 
+class InitCommand extends Command
+{
+    private const INTRO = '    ____  __  _______     ________    ____
+   / __ \/  |/  / __ \   / ____/ /   /  _/
+  / /_/ / /|_/ / /_/ /  / /   / /    / /  
+ / ____/ /  / / _, _/  / /___/ /____/ /   
+/_/   /_/  /_/_/ |_|   \____/_____/___/';
+
+    final protected function configure(): void
+    {
+        $this
+            ->setName('init')
+            ->setDescription('Init project and define it\'s resources for quick access.');
+    }
+
+    public function execute()
+    {
+
+    }
 }
