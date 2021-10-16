936a5f09e7b427b875c49a107ea334339b5f2893
Jascha030 <contact@jaschavanaalst.nl>
2021-10-16 22:27:39 +0200

    Added ProjectResource Base classes

--- src/Project/ProjectResourceAbstract.php
+++ src/Project/ProjectResourceAbstract.php
@@ -2,7 +2,30 @@
 
 namespace Jascha030\PM\Project;
 
-class ProjectResourceAbstract
+abstract class ProjectResourceAbstract implements ProjectResourceInterface
 {
+    private string $name;
 
+    private string $value;
+
+    public function __construct(string $name)
+    {
+        $this->name  = $name;
+        $this->value = '';
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
 }
--- src/Project/ProjectResourceInterface.php
+++ src/Project/ProjectResourceInterface.php
@@ -2,7 +2,14 @@
 
 namespace Jascha030\PM\Project;
 
-class ProjectResourceInterface
-{
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+interface ProjectResourceInterface
+{
+    public function getName(): string;
+
+    public function getValue(): string;
+
+    public function callResource(InputInterface $input, OutputInterface $output): int;
 }
