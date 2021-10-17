df7ff5f9a2d53f098dda462d0a5fe68d48f18b7d
Jascha030 <contact@jaschavanaalst.nl>
2021-10-17 13:26:41 +0200

    Parse config method

--- src/Service/ResourceService.php
+++ src/Service/ResourceService.php
@@ -3,18 +3,36 @@
 namespace Jascha030\PM\Service;
 
 use Jascha030\PM\Project\Category\ResourceCategory;
+use Jascha030\PM\Project\ProjectResourceAbstract;
 
 class ResourceService
 {
+    private const DATA_MAPPING = [
+        1 => 'key',
+        2 => 'category',
+        3 => 'name',
+        4 => 'value',
+        5 => 'type',
+    ];
+
     private array $definitions;
 
-    public function __construct()
-    {
-        $this->definitions = [];
+    private array $types;
 
-        $this->fetchDefinitions();
+    public function __construct(array $types =  [])
+    {
+        $this->types =  $types;
+        $this->definitions = [];
     }
 
+    public function getClassFromType(string $type): ?string
+    {
+        return $this->types[$type];
+    }
+
+    /**
+     * @return \Jascha030\PM\Project\Category\ResourceCategoryInterface[]
+     */
     public function getDefinitions(): array
     {
         return $this->definitions;
@@ -41,7 +59,53 @@ class ResourceService
                 $options[] = 'Other';
             }
 
-            $this->definitions[$key] = new ResourceCategory($definition['name'], $key, $options);
+            $this->definitions[$key] = new ResourceCategory($key, $definition['name'], $definition['type'], $options);
         }
+    }
+
+    public function parseConfig(string $path): array
+    {
+        $contents = TomlService::parseToString($path);
+        $pattern = '/\[pm.([a-z]*)]\n[ \t]*category = "(.*)"\n[ \t]*name = "(.*)"\n[ \t]*value = "(.*)"\n[ \t]*type = "(.*)"/';
+        $dataLength = preg_match_all($pattern, $contents, $matches);
+
+        if ($dataLength === null || $dataLength === false || $dataLength === 0) {
+            throw new \RuntimeException("Could not parse toml data to valid Resource objects.");
+        }
+
+        $resources  = [];
+
+        for ($index = 0; $index < $dataLength; $index++) {
+            $type = $matches[$this->getDataIndex('type')][$index];
+
+            $category = new ResourceCategory(
+                $matches[$this->getDataIndex('key')][$index],
+                $matches[$this->getDataIndex('category')][$index],
+                $type
+            );
+
+            $class = $this->getClassFromType($type);
+
+            if (! is_subclass_of($class, ProjectResourceAbstract::class)) {
+                throw new \InvalidArgumentException("Invalid type: \"{$type}\".");
+            }
+
+            $resources[$category->getKey()] = new $class(
+                $category,
+                $matches[$this->getDataIndex('name')][$index],
+                $matches[$this->getDataIndex('value')][$index],
+            );
+        }
+
+        return $resources;
+    }
+
+    private function getDataIndex(string $name): int
+    {
+        if (!isset(array_flip(self::DATA_MAPPING)[$name])) {
+            throw new \InvalidArgumentException("Invalid key name: \"{$name}\".");
+        }
+
+        return array_flip(self::DATA_MAPPING)[$name];
     }
 }
