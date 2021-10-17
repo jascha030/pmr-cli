5d7c509bc3d608206be9fb076dd944b026158fd1
Jascha030 <contact@jaschavanaalst.nl>
2021-10-17 13:26:24 +0200

    Added parseConfig test

--- tests/Service/ResourceServiceTest.php
+++ tests/Service/ResourceServiceTest.php
@@ -3,6 +3,8 @@
 namespace Jascha030\PM\Test\Service;
 
 use Jascha030\PM\Project\Category\ResourceCategoryInterface;
+use Jascha030\PM\Project\ProjectResourceInterface;
+use Jascha030\PM\Project\UrlResource;
 use Jascha030\PM\Service\ResourceService;
 use PHPUnit\Framework\TestCase;
 
@@ -10,10 +12,31 @@ class ResourceServiceTest extends TestCase
 {
     public function test__construct(): ResourceService
     {
-        $service = new ResourceService();
+        $types = [
+            UrlResource::getType() => UrlResource::class
+        ];
+
+        $service = new ResourceService($types);
         self::assertInstanceOf(ResourceService::class, $service);
 
         return $service;
+    }
+
+    /**
+     * @depends test__construct
+     */
+    public function testParseConfig(ResourceService $service): void
+    {
+        $path = dirname(__FILE__, 2) . '/Fixtures/pm.toml';
+        $parsedData = $service->parseConfig($path);
+
+        self::assertIsArray($parsedData);
+        self::assertNotEmpty($parsedData);
+        self::assertCount(2, $parsedData);
+
+        foreach($parsedData as $resource) {
+            self::assertInstanceOf(ProjectResourceInterface::class, $resource);
+        }
     }
 
     /**
